44426dbae23e2aebca1536c911ffe0749dfdbdb2
tinhat <tinhat@tutanota.de>
2025-02-11 03:19:37 -0800

    Tue Feb 11 03:19:37 AM PST 2025 added nvim and dwm conf

---
+++ neovim/nvim/lua/keymappings.lua
@@ -0,0 +1,20 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+-- Basic settings
+vim.opt.wrap = false
+
+function MakeTerminal()
+  vim.cmd("split")
+  vim.cmd("wincmd j")
+  vim.cmd("terminal")
+  vim.cmd("resize 15")
+  vim.cmd("startinsert")
+  vim.api.nvim_chan_send(vim.b.terminal_job_id, "make -j 6\n")
+end
+
+vim.api.nvim_set_keymap("n", "<leader>2", ":lua MakeTerminal()<CR>", { noremap = true, silent = true })
+
+-- Doxygen settings
+-- vim.g.DoxygenToolkit_commentType = "C"
